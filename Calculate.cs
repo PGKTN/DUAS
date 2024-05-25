79403d62b22bcd17a54529991c98f796b88950b1
PGKT <imsuha2120@outlook.kr>
2024-05-25 15:13:59 +0900

    .
    .

--- Calculate.cs
+++ Calculate.cs
@@ -18,5 +18,19 @@ namespace DUAS
         { 
             return ((sigma * Cd0) / 8) * rho * A_disk * Math.Pow(v_tip, 3);
         }
+
+        public double Get_C_fc(double reynolds, double L_avg, double M)
+        {
+            return 0.455 / (Math.Pow(Math.Log10(Get_Re_L(reynolds, L_avg, M)), 2.58) * Math.Pow((1 + 0.144 * (Math.Pow(M, 2))), 0.65));
+        }
+
+        private double Get_Re(double h)
+        { 
+            return Math.Pow(10, 6) * 
+        }
+        private double Get_Re_L(double reynolds, double L_avg, double M)
+        {
+            return reynolds * L_avg * M;
+        }
     }
 }
--- obj/Debug/net6.0/DUAS.AssemblyInfo.cs
+++ obj/Debug/net6.0/DUAS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DUAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9107445635724ebae06128fe44c5c7837f6d4ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9bdabe5dd6e5cbd66e171f912497523674c92c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DUAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DUAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
